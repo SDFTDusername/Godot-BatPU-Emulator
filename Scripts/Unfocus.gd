c9d5fd1ccbca7178a674939649d46a1577b33d00
SDFTDusername <85052868+SDFTDusername@users.noreply.github.com>
2025-07-08 22:52:33 +0200

    Made spin box unfocus when clicked off

---
+++ Scripts/Unfocus.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _input(event: InputEvent) -> void:
+	if event is not InputEventMouseButton:
+		return
+	
+	if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+		var global_rect = get_global_rect()
+		if not global_rect.has_point(event.global_position):
+			release_focus()
---
+++ Scripts/UnfocusSpinBox.gd
@@ -0,0 +1,10 @@
+extends SpinBox
+
+func _input(event: InputEvent) -> void:
+	if event is not InputEventMouseButton:
+		return
+	
+	if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+		var global_rect = get_global_rect()
+		if not global_rect.has_point(event.global_position):
+			get_line_edit().release_focus()
